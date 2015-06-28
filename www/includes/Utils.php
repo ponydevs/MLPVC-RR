ponydevs/MLPVC-RR

commit a78e1c667d269d17f30e224ecb206023f3fdcdc2

Add export feature

www/includes/Utils.php
@@ -794,17 +794,18 @@ HTML;
 	 *  where $coloumn is equal to $value
 	 * Returns null if user is not found
 	 *
-	 * If $basic is set to false, then role
-	 *  information will also be fetched
+	 * If $cols is set, only specified coloumns
+	 *  will be fetched
 	 *
 	 * @param string $value
 	 * @param string $coloumn
+	 * @param string $dbcols
+	 *
 	 * @return array|null
 	 */
-	function get_user($value, $coloumn = 'id'){
+	function get_user($value, $coloumn = 'id', $dbcols = null){
 		global $Database;
 
-		$User = array();
 		if ($coloumn === "access"){
 			$Auth = $Database->where('access', $value)->getOne('sessions');
 
@@ -813,18 +814,21 @@ HTML;
 			$value = $Auth['user'];
 		}
 
-		$User = rawquery_get_single_result($Database->rawQuery(
-			"SELECT
-				users.*,
-				roles.label as rolelabel
-			FROM users
-			LEFT JOIN roles ON roles.name = users.role
-			WHERE users.`$coloumn` = ?",array($value)));
+		if (empty($dbcols)){
+			$User = rawquery_get_single_result($Database->rawQuery(
+				"SELECT
+					users.*,
+					roles.label as rolelabel
+				FROM users
+				LEFT JOIN roles ON roles.name = users.role
+				WHERE users.`$coloumn` = ?",array($value)));
 
-		if (empty($User) && $coloumn === 'name')
-			$User = fetch_user($value);
+			if (empty($User) && $coloumn === 'name')
+				$User = fetch_user($value);
 
-		if (!empty($User) && isset($Auth)) $User['Session'] = $Auth;
+			if (!empty($User) && isset($Auth)) $User['Session'] = $Auth;
+		}
+		else $User = $Database->where($coloumn, $value)->getOne('users',$dbcols);
 
 		return $User;
 	}
@@ -1001,15 +1005,22 @@ HTML;
 	}
 
 	// Render Reservation HTML\\
-	function reservations_render($Reservations){
+	define('RETURN_ARRANGED', true);
+	function reservations_render($Reservations, $returnArranged = false){
 		$Arranged = array();
 		$Arranged['unfinished'] =
-		$Arranged['finished'] = '';
-		if (!empty($Reservations) && is_array($Reservations)){
+		$Arranged['finished'] = !$returnArranged ? '' : array();
 
-			foreach ($Reservations as $R)
-				$Arranged[(!$R['finished']?'un':'').'finished'] .= get_r_li($R);
+		if (!empty($Reservations) && is_array($Reservations)){
+			foreach ($Reservations as $R){
+				$k = (!$R['finished']?'un':'').'finished';
+				if (!$returnArranged)
+					$Arranged[$k] .= get_r_li($R);
+				else $Arranged[$k][] = $R;
+			}
 		}
+
+		if ($returnArranged) return $Arranged;
 
 		if (PERM('reservations.create')){
 			$makeRes = '<button id="reservation-btn" class=green>Make a reservation</button>';
@@ -1040,33 +1051,39 @@ HTML;
 		'obj' => 'Objects',
 		'bg' => 'Backgrounds',
 	);
-	function requests_render($Requests){
+	function requests_render($Requests, $returnArranged = false){
 		global $REQUEST_TYPES;
 
-		$Arranged = array();
+		$Arranged = array(
+			'finished' => !$returnArranged ? '' : array(),
+			'unfinished' => array(),
+		);
+		$Arranged['unfinished']['bg'] =
+		$Arranged['unfinished']['obj'] =
+		$Arranged['unfinished']['chr'] = $Arranged['finished'];
 		if (!empty($Requests) && is_array($Requests)){
-			$Arranged['unfinished'] = array();
-			$Arranged['unfinished']['bg'] =
-			$Arranged['unfinished']['obj'] =
-			$Arranged['unfinished']['chr'] =
-			$Arranged['finished'] = '';
-
 			foreach ($Requests as $R){
-				$HTML = get_r_li($R,true);
+				$HTML = $returnArranged ? get_r_li($R,true) : $R;
 
-				if ($R['finished'])
-					$Arranged['finished'] .= $HTML;
-				else $Arranged['unfinished'][$R['type']] .= $HTML;
+				if (!$returnArranged){
+					if ($R['finished'])
+						$Arranged['finished'] .= $HTML;
+					else $Arranged['unfinished'][$R['type']] .= $HTML;
+				}
+				else {
+					if ($R['finished'])
+						$Arranged['finished'][] = $HTML;
+					else $Arranged['unfinished'][$R['type']][] = $HTML;
+				}
 			}
-
+		}
+		if (!$returnArranged){
 			$Groups = '';
 			foreach ($Arranged['unfinished'] as $g => $c)
 				$Groups .= "<div class=group><h3>{$REQUEST_TYPES[$g]}:</h3><ul>{$c}</ul></div>";
 		}
-		else {
-			$Groups = '<ul></ul>';
-			$Arranged['finished'] = '';
-		}
+
+		if ($returnArranged) return $Arranged;
 
 		if (PERM('user')){
 			$makeRq = '<button id="request-btn" class=green>Make a request</button>';
@@ -1446,4 +1463,67 @@ HTML;
 		}
 
 		return $HTML;
+	}
+
+	/*
+	 * Exporting all posts
+	 * ---------------
+	 * Format:
+#########################
+<h2>Characters:</h2><br><div class="res-box"> <a href="{link}"><img src="{link}"></a> - {label}</div><br><div class="res-box"> <a href="{link}"><img src="{link}"></a> - {label}</div><br><br><br><h2>Backgrounds:</h2><br><div class="res-box"> <a href="{link}"><img src="{link}"></a> - {label}</div><br><br><h2>Objects:</h2><br>None Yet<br><br><br><h1>Finished Requests</h1><br><br><br>None Yet
+#########################
+	 */
+	function export_posts($req, $res){
+		global $REQUEST_TYPES;
+
+		$res = reservations_render($res, RETURN_ARRANGED);
+		$req = requests_render($req, RETURN_ARRANGED);
+		$nada = 'None yet';
+
+		$nameCache = array();
+
+		$Export = '<h1>List of Reservations</h1><br><br>';
+		if (empty($res['unfinished'])) $Export .= "<br>$nada";
+		else foreach ($res['unfinished'] as $r){
+			if (empty($nameCache[$r['reserved_by']])){
+				$u = get_user($r['reserved_by'],null,'username');
+				$nameCache[$r['reserved_by']] = $u['username'];
+			}
+			$username = $nameCache[$r['reserved_by']];
+			$Export .= "<div class=\"res-box\"> <a href=\"{$r['fullsize']}\"><img src=\"{$r['fullsize']}\"></a> by :icon$username: :dev$username:";
+			if (!empty($r['label'])) $Export .= " - {$r['label']}";
+			$Export .= "</div><br>";
+		}
+		$Export .= '<br><br><h1>Finished Reservations</h1><br><br>';
+		if (empty($res['finished'])) $Export .= "<br>$nada";
+		else foreach ($res['finished'] as $r){
+			if (empty($nameCache[$r['reserved_by']])){
+				$u = get_user($r['reserved_by'],null,'username');
+				$nameCache[$r['reserved_by']] = $u['username'];
+			}
+			$username = $nameCache[$r['reserved_by']];
+
+			$thumbID = parseInt(substr($r['deviation_id'],1), 36);
+
+			$Export .= "<div class=\"res-box\"> :thumb$thumbID: by :icon$username: :dev$username:</div><br>";
+		}
+		$Export .= "<br><br><h1>List of Requests</h1><br><br>";
+		foreach ($req['unfinished'] as $g => $reqs){
+			$Export .= "<br><br><br><h2>{$REQUEST_TYPES[$g]}:</h2><br>";
+			if (empty($reqs)) $Export .= $nada;
+		}
+		$Export .= "<br><br><h1>Finished Requests</h1><br><br>";
+		if (empty($req['finished'])) $Export .= "<br>$nada";
+		else foreach ($req['finished'] as $r){
+			if (empty($nameCache[$r['reserved_by']])){
+				$u = get_user($r['reserved_by'],null,'username');
+				$nameCache[$r['reserved_by']] = $u['username'];
+			}
+			$username = $nameCache[$r['reserved_by']];
+
+			$thumbID = parseInt(substr($r['deviation_id'],1), 36);
+
+			$Export .= "<div class=\"res-box\"> :thumb$thumbID: by :icon$username: :dev$username:</div><br>";
+		}
+		return $Export;
 	}

www/js/index.js
@@ -5,6 +5,17 @@ $(function(){
 		SEASON = window.SEASON,
 		EPISODE = window.EPISODE;
 
+	$('#export').on('click',function(){
+		$.post('/episode/export/S'+SEASON+'E'+EPISODE,{},function(data){
+			if (typeof data !== 'object') return console.log(data) && $w.trigger('ajaxerror');
+
+			if (data.status) $.Dialog.info('Exporting posts','<p>Here\'s the code you need to paste into the journal while in<br><em>HTML editing mode</em>, replacing what was there previously.</p><textarea style="display:block;margin:0 auto;resize:none"></textarea>',function(){
+				$('#dialogContent').find('textarea').val(data.export);
+			});
+			else $.Dialog.fail('Display voting buttons',data.message);
+		});
+	});
+
 	var $voting = $('#voting'),
 		$voteButton = $voting.find('button');
 	$voting.on('click','button',function(e){
@@ -43,7 +54,6 @@ $(function(){
 				if (data.status){
 					$voting.children('h2').nextAll().remove();
 					$voting.append(data.html);
-
 				}
 				else $.Dialog.fail('Display voting buttons',data.message);
 			});

www/do.php
@@ -320,6 +320,17 @@
 						))) respond(ERR_DB_FAIL);
 						respond(array('newhtml' => get_episode_voting($Episode)));
 					}
+					else if (preg_match('/^export\/'.EPISODE_ID_PATTERN.'$/', $data, $_match)){
+						if (!PERM('episodes.manage')) respond();
+						list($season,$episode) = array_map('intval',array_splice($_match,1,2));
+
+						$Episode = get_real_episode($season,$episode);
+						if (empty($Episode))
+							respond("There's no episode with this season & episode number");
+
+						list($req, $res) = get_posts($Episode['season'], $Episode['episode']);
+						respond(array('export' => export_posts($req, $res)));
+					}
 					else {
 						if (!PERM('episodes.manage')) respond();
 						$editing = preg_match('/^edit\/'.EPISODE_ID_PATTERN.'$/',$data,$_match);

www/scss/theme.scss
@@ -637,6 +637,7 @@ button, .btn {
 	transform-origin: 50% 100%;
 	transition: transform .1s linear, opacity .2s linear;
 
+	&.large { font-size: 22px }
 	&.blue {
 		color: $NBlueBG;
 		background-color: $NBlue;

www/views/index.php
@@ -23,12 +23,16 @@
 			<li>You MUST have an image to make a reservation! For the best quality, get your references from the episode in 1080p.</li>
 			<li>Making a reservation does NOT forbid other people from working on a pose anyway. It is only information that you are working on it, so other people can coordinate to avoid doing the same thing twice.</li>
 			<li>There are no time limits, but remember that the longer you wait, the greater the chance that someone might take your pose anyway. It's generally advised to finish your reservations before a new episode comes out.</li>
-			<li>The are no limits on the number of reservations you can make, but you need to keep your own limits and what rule #3 says in mind. The current limit for reservations are 4 at a time. You can reserve more once you've completed the previous reservation(s).</li>
-			<li>Please remember that <strong>you have to be a member of the group in order to make a reservation</strong>. The idea is to add the finished vector to the gallery, so it has to meet all of our quality requirements.</li>
+			<li>The current limit for reservations are 4 at a time. You can reserve more once you've completed the previous reservation(s).</li>
+			<li>Please remember that <strong>you have to be a member of the group in order to make a reservation</strong>. The idea is to add the finished vector to our gallery, so it has to meet all of our quality requirements.</li>
 		</ol>
 	</div>
-
-<?php   echo reservations_render($Reservations);
+<?php   if (PERM('episodes.manage')){ ?>
+	<p class=align-center>
+		<button id=export class="typcn typcn-export large blue">Export posts in journal format</button>
+	</p>
+<?php   }
+		echo reservations_render($Reservations);
 		echo requests_render($Requests); ?>
 	<script>var SEASON = <?=$CurrentEpisode['season']?>, EPISODE = <?=$CurrentEpisode['episode']?>;</script>
 <?php

commit e3779a710465ecfe09997023ee3826d8559f69e5

Fix requests displaying as ArrayArrayArray & URL input usage via keyboard
A bug (missing !) in the previous commit a78e1c6 caused requests to be rendered as a an array instead of a string
The outgoing link check introduced in commit 8045170 made it next to impossible to use the URL input field via the keyboard due to its value being constantly reset on keyup

www/includes/Utils.php
@@ -1063,7 +1063,7 @@ HTML;
 		$Arranged['unfinished']['chr'] = $Arranged['finished'];
 		if (!empty($Requests) && is_array($Requests)){
 			foreach ($Requests as $R){
-				$HTML = $returnArranged ? get_r_li($R,true) : $R;
+				$HTML = !$returnArranged ? get_r_li($R,true) : $R;
 
 				if (!$returnArranged){
 					if ($R['finished'])

www/js/index.js
@@ -278,6 +278,7 @@ $(function(){
 			})
 		});
 		$formImgInput.on('keyup change paste',imgCheckDisabler);
+		var outgoing =  /^https?:\/\/www\.deviantart\.com\/users\/outgoing\?/;
 		function imgCheckDisabler(disable){
 			var prevurl = $formImgInput.data('prev-url'),
 				samevalue = typeof prevurl === 'string' && prevurl.trim() === $formImgInput.val().trim();
@@ -285,8 +286,11 @@ $(function(){
 			if (disable === true || samevalue) $formImgCheck.attr('title', 'You need to change the URL before chacking again.');
 			else $formImgCheck.removeAttr('title');
 
-			if (disable.type === 'keyup')
-				$formImgInput.val($formImgInput.val().replace('http://www.deviantart.com/users/outgoing?',''));
+			if (disable.type === 'keyup'){
+				var val = $formImgInput.val();
+				if (val.test(outgoing))
+					$formImgInput.val($formImgInput.val().replace(outgoing,''));
+			}
 		}
 		$formImgCheck.on('click',function(e){
 			e.preventDefault();