466eec514498f35ef92614c8a095253ba2018b09
SeinopSys <seinopsys@gmail.com>
2020-01-05 23:11:01 +0100

    Update Travis configuration for PostgreSQL 12

--- db/migrations/20180319233942_change_session_data_column_type_to_jsonb.php
+++ db/migrations/20180319233942_change_session_data_column_type_to_jsonb.php
@@ -4,10 +4,10 @@ use Phinx\Migration\AbstractMigration;
 
 class ChangeSessionDataColumnTypeToJsonb extends AbstractMigration {
   public function up() {
-    $this->query('ALTER COLUMN data TYPE jsonb;');
+    $this->query('ALTER TABLE sessions ALTER COLUMN data TYPE jsonb;');
   }
 
   public function down() {
-    $this->query('ALTER COLUMN data TYPE text;');
+    $this->query('ALTER TABLE sessions ALTER COLUMN data TYPE text;');
   }
 }
